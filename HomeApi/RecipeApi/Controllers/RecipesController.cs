e6300cb93a5afa824b74cedc889471949485f2ca
Ian Wesa <ian.wesa@gmail.com>
2020-01-09 00:00:17 -0500

    api(RecipeApi): Created recipe api under the home api solution. Will be used to store and retrieve recipes for cooking

---
+++ HomeApi/RecipeApi/Controllers/RecipesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Models;
+
+namespace RecipeApi.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipeContext _context;
+
+        public RecipesController(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recipes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipeItems()
+        {
+            return await _context.RecipeItems.ToListAsync();
+        }
+
+        // GET: api/Recipes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(long id)
+        {
+            var recipe = await _context.RecipeItems.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        // PUT: api/Recipes/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(long id, Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipes
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.RecipeItems.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+
+        // DELETE: api/Recipes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Recipe>> DeleteRecipe(long id)
+        {
+            var recipe = await _context.RecipeItems.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.RecipeItems.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return recipe;
+        }
+
+        private bool RecipeExists(long id)
+        {
+            return _context.RecipeItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HomeApi/RecipeApi/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
